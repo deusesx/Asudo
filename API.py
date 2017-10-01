f48e777bcee94b1f1d6fc1e9a6799c5460264e79
deusesx <psbhackathon010@yandex.ru>
2017-10-01 10:41:34 +0400

    profiles works

--- API.py
+++ API.py
@@ -1,21 +1,24 @@
 import pandas as pd
 from flask import Flask
+import json
 
 DB_ADDRESS = 'postgresql://postgres@78.24.221.184:5432/asudo?user=postgres&password=demo123'
 app = Flask(__name__)
 
 @app.route('/user/{user_id}/', methods=['GET'])
 def get_user(user_id):
-    user_query = "SELECT * FROM profiles WHERE user_id='{0}'"
-    try:
-        user_profile = pd.read_sql_query(user_query.format(user_id), 
+    user_query = "SELECT * FROM profiles WHERE uid={0}"
+    user_profile = pd.read_sql_query(user_query.format(user_id), 
                                      con=DB_ADDRESS)
-        result = {}
-        for col in user_profile.columns:
-            result[col] = user_profile[col].values[0]
-        return json.dumps(result), 200
-    except:
-        return 500
+    result = {}
+    for col in user_profile.columns:
+        t = user_profile[col].values
+        if len(t) > 0:
+            result[col] = t[0]
+        else:
+            return json.dumps({'error': 'user {0} not found'.format(user_id)}), 500
+    print(result)
+    return json.dumps(result), 200
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='777')
